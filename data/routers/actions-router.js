630c5690bbf685e636b55b8d7e2c138b80493fb7
Andrew <acarmstrong98@gmail.com>
2019-03-23 13:21:49 -0400

    added put post and delete

--- data/routers/actions-router.js
+++ data/routers/actions-router.js
@@ -4,6 +4,8 @@ const Actions = require('../helpers/actionModel.js')
 
 const router = express.Router()
 
+// GET
+
 router.get('/', async (req, res) => {
     try {
       const actions = await Actions.get()
@@ -15,6 +17,8 @@ router.get('/', async (req, res) => {
     }
   })
   
+// GET BY ID
+
   router.get('/:id', async (req, res) => {
     try {
       const action = await Actions.get(req.params.id)
@@ -28,4 +32,60 @@ router.get('/', async (req, res) => {
         .status(500)
         .json({ error: 'The action information could not be retrieved.' })
     }
+  })
+
+  // POST
+
+  router.post('/', async (req, res) => {
+    if (req.body && req.body.project_id && req.body.description && req.body.notes)
+      try {
+        const action = await Actions.insert(req.body)
+        res.status(201).json(action)
+      } catch (error) {
+        res.status(500).json({
+          error: 'There was an error while saving the action to the database'
+        })
+      }
+    else
+      res.status(400).json({
+        errorMessage:
+          'Please provide description, notes, and project id for the action.'
+      })
+  })
+
+  // DELETE
+  
+  router.delete('/:id', async (req, res) => {
+    try {
+      const action = await Actions.remove(req.params.id)
+      action
+        ? res.status(200).json(action)
+        : res
+            .status(404)
+            .json({ message: 'The action with the specified ID does not exist.' })
+    } catch (error) {
+      res.status(500).json({ error: 'The action could not be removed' })
+    }
+  })
+
+  // PUT
+  
+  router.put('/:id', async (req, res) => {
+    if (req.body)
+      try {
+        const action = await Actions.update(req.params.id, req.body)
+        action
+          ? res.status(200).json(action)
+          : res.status(404).json({
+              message: 'The action with the specified ID does not exist.'
+            })
+      } catch (error) {
+        res
+          .status(500)
+          .json({ error: 'The action information could not be modified.' })
+      }
+    else
+      res.status(400).json({
+        errorMessage: 'Please provide content for the action change.'
+      })
   })
