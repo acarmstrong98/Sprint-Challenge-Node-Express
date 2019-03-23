8f38611acab7db7126247137241a1f6c2daa3576
Andrew <acarmstrong98@gmail.com>
2019-03-23 13:12:23 -0400

    project start with settup

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 // play this: https://www.youtube.com/watch?v=d-diB65scQU
 
 // code away!
+
+const express = require('express')
+const ProjectsRouter = require('./data/routers/projectsRouter.js')
+const ActionsRouter = require('./data/routers/actionsRouter.js')
+const server = express()
+server.use(express.json())
+// server.use('/api/posts', ProjectRouter)
+// server.use('/api/users', ActionRouter)
+const PORT = 5000
+
+server.get('/', (req, res) => {
+    res.send(`<h2>Welcome to the API</h2>`)
+  })
+  
+  server.listen(PORT, _ =>
+    console.log(`SERVER RUNNING ON http://localhost:${PORT}`)
+  )
